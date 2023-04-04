e87f69188e28a62e7093ffcaeda55a02c1e9a634
chrsflkr <78263907+chrsflkr@users.noreply.github.com>
2023-04-04 16:21:27 +0200

    working-BackEnd

---
+++ frontend/public/script.js
@@ -0,0 +1,22 @@
+
+
+console.log("error")  
+
+fetch(`http://127.0.0.1:9000/data`)
+.then(response =>{
+   console.log(response)
+   if (response.status === 201) {
+      console.log('OK')
+   }
+   return response.json()
+   })
+.then(responseJson => {
+  const data = responseJson
+  data.forEach(element => {
+    console.log(element.name)
+    document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`
+)});
+})
+
+
+
