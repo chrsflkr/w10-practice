fdee267aa24ff2e8f799f015057a341eab350ee3
chrsflkr <78263907+chrsflkr@users.noreply.github.com>
2023-04-05 20:16:30 +0200

    upload_function

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,91 @@
-const express = require('express') //import
-const path= require('path')
-const fs = require("fs")
-const app = express()
-const port = 9000
+const express = require("express"); //import
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const fileUpload = require('express-fileupload')
+const port = 9000;
+
+app.use(fileUpload());
 
 // add point
-app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+});
 
-app.get('/pic/exam.png', (req, res) => {
-	res.sendFile(path.join(`${__dirname}/../frontend/pic/exam.png`))
- })
+app.get("/pic/exam.png", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/pic/exam.png`));
+});
 
+app.use("/public", express.static(`${__dirname}/../frontend/public`));
 
+app.get("/data", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/data/data.json`));
+});
 
-app.use('/public', express.static(`${__dirname}/../frontend/public`))
-
-app.get('/data', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/data/data.json`))
-})
-
-app.get('/data/:id', (req, res) => {
-  
+app.get("/data/:id", (req, res) => {
   //console.log(req.params)
-try{
-  const searchId = parseInt(req.params.id)
-  //console.log(searchId)
+  try {
+    const searchId = parseInt(req.params.id);
+    //console.log(searchId)
 
-  if (isNaN(searchId)  ) {
-    res.status(418).send("not a number")
-  } else {
-    // res.send("ok")
-    fs.readFile(`data/data.json`, (error, data) => {
-      let result= null
-      const fileData = JSON.parse(data)
-      //console.log(fileData)
-      for (let i =0; i < fileData.length; i++) {
-        const element= fileData[i]
-        if (element.id === searchId) {        
-          console.log(element) 
-          result = element
+    if (isNaN(searchId)) {
+      res.status(418).send("not a number");
+    } else {
+      // res.send("ok")
+      fs.readFile(`${__dirname}data/data.json`, (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          let result = null;
+          const fileData = JSON.parse(data);
+          //console.log(fileData)
+          for (let i = 0; i < fileData.length; i++) {
+            const element = fileData[i];
+            if (element.id === searchId) {
+              console.log(element);
+              result = element;
+            }
+          }
+          if (result === null) {
+            res.status(404).send("not found user");
+          } else {
+            res.send(result);
+          }
         }
-      } 
-      if (result === null) {
-        res.status(404).send("not found user")
-      } else {
-        res.send(result)
-      }
-    })
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("we have a problem");
+  }
+});
+
+app.post('/upload', (req, res) => {
+  let uploadedFile;
+  let savePath;
+  let imgName;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
   }
 
-  
-}catch(error){
-  console.log(error)
-  res.send("we have a problem")
-}
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  uploadedFile = req.files.image;
+  imgName = req.body.name;
+  savePath = `${__dirname}/../frontend/public/${imgName}.jpg`;
 
-})
+  // Use the mv() method to place the file somewhere on your server
+  uploadedFile.mv(savePath, (err) => {
+    if (err)
+      return res.status(500).send(err);
 
-
+    res.json(imgName); //imgName= pelda
+  });
+});
 
 
 app.listen(port, () => {
-  console.log(`http://127.0.0.1:${port}`)
-})
+  console.log(`http://127.0.0.1:${port}`);
+});
+
+//Users/chrs/Documents/codecool/practices/w10
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,22 +1,46 @@
+console.log('loaded')
 
+const formComponent = () => `
+  <form>
+    <input type="text" placeholder="Image name" name="name">
+    <input type="file" name="file">
+    <button>SEND</button>
+  </form>
+`
+const rootElement = document.querySelector('#root')
 
-console.log("error")  
+rootElement.insertAdjacentHTML('beforeend', formComponent())
 
-fetch(`http://127.0.0.1:9000/data`)
-.then(response =>{
-   console.log(response)
-   if (response.status === 201) {
-      console.log('OK')
-   }
-   return response.json()
+const formElement =document.querySelector('form')
+
+formElement.addEventListener('submit', (event) => {
+   event.preventDefault()
+   console.log("Submit?")
+
+   const formData = new FormData()
+   formData.append('name', document.querySelector(`input[type='text']`).value) 
+   formData.append('image', document.querySelector(`input[type='file']`).files[0])
+   //formData.append('age', '33')
+
+   fetch('/upload', {
+      method: 'POST',
+      body: formData
    })
-.then(responseJson => {
-  const data = responseJson
-  data.forEach(element => {
-    console.log(element.name)
-    document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`
-)});
+
+   .then(res => {
+      if (res.status === 200) {
+         console.log('success')
+         return res.json()
+      } else {
+         console.log('ERORR!!!')
+      }
+   })
+   .then(resJson => {   //resData= "pelda"
+      console.log(resData)
+      rootElement.insertAdjacentHTML('beforeend'), `
+      <img src="./public/${resData}.jpg"> `
+   })
+
+   .catch(error => console.log(error))
 })
-
-
 
